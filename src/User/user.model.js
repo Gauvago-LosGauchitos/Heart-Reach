245544259078ea72c0233e85ca0aa60a03b32c98
cgarcia-2022025 <cgarcia-2022025@kinal.edu.gt>
2024-05-31 08:50:00 -0600

    adelante user y mongo db atlas arreglado

---
+++ src/User/user.model.js
@@ -0,0 +1,58 @@
+'use strict'
+
+import { Schema, model } from "mongoose"
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required.']
+    },
+    surname: {
+        type: String,
+        required: [true, 'Surname is required.']
+    },
+    dpi: {
+        type: Number,
+        minLength: [13, 'The DPI must be 13 digits long.'],
+        maxLength: [13, 'The DPI must be 13 digits long.'],
+        required: [true, 'DPI is required.']
+    },
+    username: {
+        type: String,
+        unique: [true, 'Username must be unique.'],
+        lowercase: true,
+        required: [true, 'Username is required.']
+    },
+    password: {
+        type: String,
+        minLength: [8, 'Password min must be 8 characters.'],
+        required: [true, 'Password is required.']
+    },
+    email: {
+        type: String,
+        unique: [true, 'Email already exist.'],
+        required: [true, 'Email is required.']
+    },
+    phone: {
+        type: String,
+        minLength: [8, 'The phone number must be 8 digits long.'],
+        maxLength: [8, 'The phone number must be 8 digits long.'],
+        required: [true, 'Phone is required.']
+    },
+    habilities: {
+        type: String,
+        required: false
+    },
+    imageProfile: {
+        type: [String],
+        required: false
+    },
+    role: {
+        type: String,
+        uppercase: true,
+        enum: ['ADMIN', 'ADMIN', 'ASOCIATION', 'USER'],
+        required: [true, 'Role is required.']
+    }
+})
+
+export default model('user', userSchema);
---
+++ src/utils/validator.js
@@ -0,0 +1,80 @@
+'use strict'
+
+import { hash/*, compare*/ } from 'bcrypt';
+//encriptar cualquier cosa
+export const encrypt = (value) => {
+    try {
+        return hash(value, 10);
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+}
+
+//Validar encriptaciones
+/*
+export const checkEncrypt = async (value, valueEncrypt) => {
+    try {
+        return await compare(value, valueEncrypt);
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+}
+*/
+//validar actualizacion
+export const checkUpdate = (data, role) => {
+    
+    if (role == 'ADMIN-PLATFORM') {
+        if (
+            Object.entries(data).length === 0 ||
+            data.password || //si cambia la password devuelve false
+            data.password == '' //si envia la password vacia
+        ) return false;
+        return true;
+    } else {
+        if (
+            Object.entries(data).length === 0 ||
+            data.password || //si cambia la password devuelve false
+            data.password == '' || //si envia la password vacia
+            data.role || //si modifica el rol
+            data.role == '' //si deja el rol vacio.
+        ) return false;
+        return true;
+    }
+
+}
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        let { user } = req;//req que ya tenemos
+        if (!user || user.role !== 'ADMIN') return res.status(403).send({ message: `You dont have access. | username: ${user.username}` });
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(403).send({ message: `Unauthorization role` })
+    }
+}
+
+export const isAsociation = async (req, res, next) => {
+    try {
+        let { user } = req;//req que ya tenemos
+        if (!user || user.role !== 'ASOCIATION') return res.status(403).send({ message: `You dont have access. | username: ${user.username}` });
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(403).send({ message: `Unauthorization role` })
+    }
+}
+
+export const isUser = async (req, res, next) => {
+    try {
+        let { user } = req;//req que ya tenemos
+        if (!user || user.role !== 'USER') return res.status(403).send({ message: `You dont have access. | username: ${user.username}` });
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(403).send({ message: `Unauthorization role` })
+    }
+}
+
