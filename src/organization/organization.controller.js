f2659cd9e7f8c1653e6cb2d4dd3969c10dc2c18b
cdelacruz-2019262 <cdelacruz-2019262@kinal.edu.gt>
2024-06-01 16:40:57 -0600

    puleito

--- src/organization/organization.controller.js
+++ src/organization/organization.controller.js
@@ -1,4 +1,5 @@
 import Organization from './organization.model.js'
+import User from '../User/user.model.js'
 
 export const test = (req, res) => {
     console.log('test panoli')
@@ -20,14 +21,18 @@ export const orgRequest = async (req, res) => {
                     address: data.address
                 }
             ]
+        })
+        if (req.user.role === 'ADMIN-ASOCIATION' || req.user.role === 'ADMIN') return res.status(400).send({
+            message: 'You have an organization or you are an admin.'
         });
-        console.log(data)
+
         if (existingOrg) {
             return res.status(400).send({
                 message: 'The org Request request already exists or repeated data. The data that cannot be repeated is the name, address and telephone number.'
             });
         }
         let orgRequest = new Organization(data);
+        console.log(data)
         await orgRequest.save();
         return res.send({
             message: '¡The hotel Request has been successfully registered!'
@@ -35,5 +40,37 @@ export const orgRequest = async (req, res) => {
     } catch (err) {
         console.log(err)
         return res.status(500).send({ message: 'Error solicitating the organization' })
+    }
+}
+
+export const orgConfirm = async (req, res) => {
+    try {
+        let data = req.body
+        let org = await Organization.findOne({ nameHotel: data.nameHotel})
+        let orgRequest = await Organization.findOneAndUpdate(
+            {
+                nameHotel: data.nameHotel
+            }, {
+            role: "ACEPTADO"
+        }
+        );
+        let newOrgAdmin = await User.findById(org.owner)
+        console.log(newOrgAdmin.role)
+        if (newOrgAdmin.role === 'ADMIN') {
+            return res.status(401).send({ message: 'And admin can`t have an organization' })
+        }
+        else if (newOrgAdmin.role === 'ADMIN-ASOCIATION') {
+            return res.status(401).send({ message: 'This User have an organization' })
+        }else {
+            await User.findByIdAndUpdate({
+                _id: orgRequest.owner
+            }, {
+                role: "ADMIN-ASOCIATION"
+            })
+            return res.send({message: 'Organization correctly added'})
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({ message: 'Error acepting the organization' })
     }
 }
--- src/organization/organization.routes.js
+++ src/organization/organization.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { test, orgRequest } from './organization.controller.js'
+import { test, orgRequest, orgConfirm } from './organization.controller.js'
 import {validateJwt} from "../middlewares/validate-jwt.js"
 
 const api = Router();
 
 api.get('/test', test);
 api.post('/request' ,[validateJwt], orgRequest);
+api.post('/confirm', orgConfirm)
 
 export default api
