710f7de20558ae4b9fa363755a6630b6ff1a7c3e
dzabala-2019001 <dzabala-2019001@kinal.edu.gt>
2024-06-01 19:09:02 -0600

    controlador basico de Volunteering

--- src/volunteering/volunteering.controller.js
+++ src/volunteering/volunteering.controller.js
@@ -1,4 +1,5 @@
 import Volunteering from "./volunteering.model.js"
+import { checkUpdateV } from "../utils/validator.js"
 
 //testeo
 export const test = (req, res)=>{
@@ -7,7 +8,6 @@ export const test = (req, res)=>{
 }
 
 //Register
-
 export const registerV = async (req, res) => {
     try {
         let data = req.body;
@@ -22,3 +22,52 @@ export const registerV = async (req, res) => {
         return res.status(500).send({ message: 'Error registering the Volunteering', err: err });
     }
 }
+
+//Eliminar
+export const deleteV = async(req, res)=>{
+    try {
+        let { id } = req.params;
+        if (!id) {
+            return res.status(400).send({ message: 'ID is required' });
+        }
+        let deletedVolunteering = await Volunteering.findOneAndDelete({_id: id});
+        if(!deletedVolunteering) return res.status(404).send({message: 'Volunteering no encontrada y no eliminada'}); 
+        return res.send({message: `Volunteering con nombre  ${deletedVolunteering.title} eliminado exitosamente`});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message: 'Error al eliminar a Volunteering'});
+    }
+}
+
+//Listar
+export const listarVolunteering = async (req, res) => {
+    try {
+        let data = await Volunteering.find().select('-__v').select('-_id');
+        return res.send({ data });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ message: 'The information cannot be obtained.' });
+    }
+}
+
+//Update
+
+export const UpdateV = async (req, res) => {
+    try{
+        let {id} = req.params
+        let data = req.body
+        let update = checkUpdateV(data, id)
+        if(!update) return res.status .send({message: `Volunteering actualizado exitososamente`})
+        let  updateVolunteering = await Volunteering.findOneAndUpdate(
+            {_id: id},
+            data,
+            {new: true}
+        )
+        if(!updateVolunteering) return res.status(404).send({ message: `Volunteering c no encontrado`})
+        return res.send({message: `Volunteering actualizado exitoso`})
+    }   catch(error){
+        console.error(error);
+        return res.status(500).send({message: 'Error al actualizar Volunteering', error: error.message})
+    }     
+}
+
