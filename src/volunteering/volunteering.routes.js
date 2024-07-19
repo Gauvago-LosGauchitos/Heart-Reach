f8ac4cfa80ecef7376ddafd43a2ba0092a32c70d
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-07-18 18:35:27 -0600

    cambios

--- src/volunteering/volunteering.routes.js
+++ src/volunteering/volunteering.routes.js
@@ -15,6 +15,6 @@ api.get('/messages/:chatRoom', messages)
 api.get('/getTypesOfVolunteering', [validateJwt], getVolunteeringTypes)
 api.get('/actualizate/status', updateStatus)
 api.put('/assignVolunteering', [validateJwt], assignVolunteering)
-api.get('/volunteering/disponible', listarVolunteeringDisponiblesEnCurso)
+api.get('/volunteering/disponible-encurso', listarVolunteeringDisponiblesEnCurso)
 api.get('/getVolunteering/:id', findVolunteer)
 export default api
