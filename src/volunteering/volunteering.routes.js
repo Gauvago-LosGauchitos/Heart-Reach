1015eb7aa42a65d79f7992dd360f6fea154ac986
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-07-18 18:13:33 -0600

    cambios en voluntariado

--- src/volunteering/volunteering.routes.js
+++ src/volunteering/volunteering.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { UpdateV, addType, deleteV, listarVolunteering, registerV, test, messages, getVolunteeringTypes, updateStatus, assignVolunteering } from './volunteering.controller.js'
+import { UpdateV, addType, deleteV, listarVolunteering, registerV, test, messages, getVolunteeringTypes, updateStatus, assignVolunteering, findVolunteer } from './volunteering.controller.js'
 import { validateJwt } from "../middlewares/validate-jwt.js";
 import { upload } from '../utils/multerConfig.js';
 
@@ -8,12 +8,13 @@ const api = Router();
 api.get('/test', test);
 api.post('/registerV',[validateJwt, upload.single('img')], registerV)
 api.delete('/deleteV/:id', deleteV)
-api.get('/listarVolunteering', listarVolunteering)
+api.get('/listarVolunteering', [validateJwt], listarVolunteering)
 api.put('/UpdateV/:id', UpdateV)
 api.put('/add/Type', addType)
 api.get('/messages/:chatRoom', messages)
 api.get('/getTypesOfVolunteering', [validateJwt], getVolunteeringTypes)
 api.get('/actualizate/status', updateStatus)
 api.put('/assignVolunteering', [validateJwt], assignVolunteering)
+api.get('/getVolunteering/:id', findVolunteer)
 
 export default api
